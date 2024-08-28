7f1aca6ca633857aa18adb996b09810553406620
jyoti pal <paljyoti0129@gmail.com>
2024-08-28 08:19:37 +0530

    added routes

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import Navigation from "@/components/site/Navigation";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="h-full">
-      <ClerkProvider appearance={{ baseTheme: dark }}>
-        <Navigation />
-        {children}
-      </ClerkProvider>
-    </main>
+    <ClerkProvider appearance={{ baseTheme: dark }}>{children}</ClerkProvider>
   );
 };
 
--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { pricingCards } from "@/lib/constants";
+import { Check } from "lucide-react";
+import Link from "next/link";
 
+import Image from "next/image";
+import clsx from "clsx";
 export default function Home() {
   return (
     <>
@@ -7,7 +19,7 @@ export default function Home() {
         {/* grid */}
 
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
-        <p className="text-center">Run you agency, in one place</p>
+        <p className="text-center">Run your agency, in one place</p>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
           <h1 className="text-9xl font-bold text-center md:text-[300px] ">
             Nexa
@@ -32,7 +44,52 @@ export default function Home() {
           {"you'r"} not <br /> ready to commit you can get started for free
         </p>
 
-        <div></div>
+        <div className="flex  justify-center gap-4 flex-wrap mt-6">
+          {pricingCards.map((card) => (
+            <Card
+              key={card.title}
+              className={clsx("w-[300px] flex flex-col justify-between", {
+                "border-2 border-primary": card.title === "Unlimited Saas",
+              })}
+            >
+              <CardHeader>
+                <CardTitle
+                  className={clsx("", {
+                    "text-muted-foreground": card.title !== "Unimited Saas",
+                  })}
+                >
+                  {card.title}
+                </CardTitle>
+                <CardDescription>{card.description}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="text-4xl font-bold">{card.price}</span>
+                <span className="text-muted-foreground">/m</span>
+              </CardContent>
+              <CardFooter className="flex flex-col items-start gap-4">
+                <div>
+                  {card.features.map((feature) => (
+                    <div key={feature} className="flex gap-2 items-center">
+                      <Check />
+                      <p className="">{feature}</p>
+                    </div>
+                  ))}
+                </div>
+                <Link
+                  href={`/agency?plan=${card.priceId}`}
+                  className={clsx(
+                    "w-full text-center bg-primary p-2 rounded-md",
+                    {
+                      "!bg-muted-foreground": card.title !== "Unlimited Saas",
+                    }
+                  )}
+                >
+                  Get Started
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
       </section>
     </>
   );
